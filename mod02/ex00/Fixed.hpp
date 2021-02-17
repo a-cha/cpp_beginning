a782002b0c251c12b6e38d4514aa496646666005
Anton Chaparin <chaparin.anton@gmail.com>
2021-02-17 14:33:35 +0300

    check smth

---
+++ mod02/ex00/Fixed.hpp
@@ -0,0 +1,15 @@
+//
+// Created by Skipjack Adolph on 2/17/21.
+//
+
+#ifndef EX00_FIXED_HPP
+#define EX00_FIXED_HPP
+
+
+class Fixed
+{
+
+};
+
+
+#endif //EX00_FIXED_HPP
---
+++ mod02/ex01/Fixed.hpp
@@ -0,0 +1,15 @@
+//
+// Created by Skipjack Adolph on 2/17/21.
+//
+
+#ifndef EX01_FIXED_HPP
+#define EX01_FIXED_HPP
+
+
+class Fixed
+{
+
+};
+
+
+#endif //EX01_FIXED_HPP
