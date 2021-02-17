a56b6f8a601891a826e82200f1b6bf618d51a289
Anton Chaparin <chaparin.anton@gmail.com>
2021-02-17 20:16:09 +0300

    ex01 done

--- mod02/ex01/Fixed.cpp
+++ mod02/ex01/Fixed.cpp
@@ -3,3 +3,70 @@
 //
 
 #include "Fixed.hpp"
+#include <iostream>
+#include <cmath>
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called\n";
+	this->rawBits = 0;
+}
+
+Fixed::Fixed(const Fixed &other)
+{
+	std::cout << "Copy constructor called\n";
+	*this = other;
+}
+
+Fixed &Fixed::operator=(const Fixed &other)
+{
+	std::cout << "Assignation operator called\n";
+	if (this != &other)
+		this->rawBits = other.getRawBits();
+	return *this;
+}
+
+Fixed::Fixed(const int value)
+{
+	std::cout << "Int constructor called\n";
+	this->rawBits = value << fractBits;
+}
+
+Fixed::Fixed(const float value)
+{
+	std::cout << "Float constructor called\n";
+	rawBits = (int)roundf(value * (1 << fractBits));
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called\n";
+}
+
+int Fixed::getRawBits() const
+{
+//	std::cout << "getRawBits member function called\n";
+	return rawBits;
+}
+
+void Fixed::setRawBits(const int raw)
+{
+	std::cout << "setRawBits member function called\n";
+	this->rawBits = raw;
+}
+
+int Fixed::toInt() const
+{
+	return rawBits >> fractBits;
+}
+
+float Fixed::toFloat() const
+{
+	return (float)rawBits / (1 << fractBits);
+}
+
+std::ostream& operator<<(std::ostream& os, const Fixed& other)
+{
+	os << other.toFloat();
+	return os;
+}
--- mod02/ex01/Fixed.hpp
+++ mod02/ex01/Fixed.hpp
@@ -2,14 +2,29 @@
 // Created by Skipjack Adolph on 2/17/21.
 //
 
-#ifndef EX01_FIXED_HPP
-#define EX01_FIXED_HPP
+#ifndef EX00_FIXED_HPP
+#define EX00_FIXED_HPP
 
+#include <iostream>
 
 class Fixed
 {
-
+private:
+	int rawBits;
+	static const int fractBits = 8;
+public:
+	Fixed();
+	Fixed(const Fixed& other);
+	Fixed& operator=(const Fixed& other);
+	explicit Fixed(const int value);
+	explicit Fixed(const float value);
+	virtual ~Fixed();
+	int getRawBits() const;
+	void setRawBits(const int raw);
+	int toInt() const;
+	float toFloat() const;
 };
 
+std::ostream& operator<<(std::ostream& os, const Fixed& other);
 
-#endif //EX01_FIXED_HPP
+#endif //EX00_FIXED_HPP
