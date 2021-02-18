72e6ad20eff160e338bbd0d85bddf66b9d1c7bee
Anton Chaparin <chaparin.anton@gmail.com>
2021-02-18 13:16:22 +0300

    add main func

---
+++ mod03/ex00/main.cpp
@@ -0,0 +1,20 @@
+#include "FragTrap.hpp"
+#include "print.hpp"
+
+int main()
+{
+	FragTrap robo("Bringer of death");
+	std::string target("Sucker");
+
+	std::cout << BOLD "It's time to battle of the century!\n" << STD;
+	robo.meleeAttack(target);
+	robo.rangedAttack(target);
+	robo.vaulthunter_dot_exe(target);
+	robo.takeDamage(50);
+	robo.vaulthunter_dot_exe(target);
+	robo.takeDamage(70);
+	robo.beRepaired(60);
+	robo.vaulthunter_dot_exe(target);
+	std::cout << BOLD "Ohh... That was good\n" << STD;
+	return 0;
+}
