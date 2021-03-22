5c563bcd1e1d4456783b3d336e7a3089352a0cb2
Anton Chaparin <chaparin.anton@gmail.com>
2021-03-22 12:45:38 +0300

    add list cont to tests

--- mod08/ex00/main.cpp
+++ mod08/ex00/main.cpp
@@ -3,47 +3,86 @@
 #include "print.hpp"
 #include <vector>
 #include <list>
-#include <set>
 
 int main() {
-	std::vector<int> myVector(4);
-	std::vector<int>::size_type sizeMyVector = myVector.size();
+	{
+		std::vector<int> myVector(4);
+		std::vector<int>::size_type sizeMyVector = myVector.size();
 
-	std::cout << BOLD "Playing with vector" STD << std::endl;
-	for (int i = 0; i < sizeMyVector; i++)
-		myVector[i] = i + 1;
-	myVector.push_back(5);
-	myVector.push_back(6);
-	myVector.push_back(7);
+		std::cout << BOLD "Playing with " BLUE "vector" STD << std::endl;
+		for (std::vector<int>::size_type i = 0; i < sizeMyVector; i++)
+			myVector[i] = i + 1;
+		myVector.push_back(5);
+		myVector.push_back(6);
+		myVector.push_back(7);
 
-//	There is no possible operation (add extra values by iterator)
-	sizeMyVector = myVector.size();
-	std::vector<int>::iterator iter = myVector.end();
-	for (int i = 0; i < 3; i++) {
-		*iter = i + sizeMyVector;
-		iter++;
+//		This is no possible operation (add extra values by iterator .end()++)
+		sizeMyVector = myVector.size();
+		std::vector<int>::iterator iter = myVector.end();
+		for (int i = 0; i < 3; i++) {
+			*iter = i + sizeMyVector;
+			iter++;
+		}
+
+//		Change values & print them
+		iter = myVector.begin();
+		while (iter != myVector.end()) {
+			*iter = *iter - 3;
+			std::cout << *iter << std::endl;
+			iter++;
+		}
+
+		try {
+			std::cout << BOLD "Testing " GREEN "easyfind " STD BOLD "with vector" STD << std::endl;
+			std::cout << *easyfind(myVector, 2) << std::endl;
+			std::cout << *easyfind(myVector, 0) << std::endl;
+			std::cout << *easyfind(myVector, -1) << std::endl;
+			std::cout << *easyfind(myVector, 100) << std::endl;
+			std::cout << *easyfind(myVector, 1) << std::endl;
+		} catch (std::exception &exc) {
+			std::cerr << RED << exc.what() << STD << std::endl;
+		}
 	}
 
-//	Change values & print them
-	iter = myVector.begin();
-	while (iter != myVector.end()) {
-		*iter = *iter - 3;
-		std::cout << *iter << std::endl;
-		iter++;
-	}
+	{
+		std::list<int> myList(2);
+		std::list<int>::size_type sizeMyList = myList.size();
 
-	try {
-		std::cout << std::endl << BOLD "Testing easyfind" STD << std::endl;
-		std::cout << *easyfind(myVector, 2) << std::endl;
-		std::cout << *easyfind(myVector, 0) << std::endl;
-		std::cout << *easyfind(myVector, -1) << std::endl;
-		std::cout << *easyfind(myVector, 100) << std::endl;
-		std::cout << *easyfind(myVector, 1) << std::endl;
-	}
-	catch (std::exception & exc) {
-		std::cout << RED << exc.what() << STD << std::endl;
-	}
+		std::cout << std::endl << BOLD "Playing with " BLUE "list" STD
+				  << std::endl;
+//		for (int i = 0; i < sizeMyVector; i++)
+//			myList[i] = i + 1;
+		myList.push_back(5);
+		myList.push_back(6);
+		myList.push_back(7);
 
+		std::list<int>::iterator iter = myList.end();
+//		It is "overflow" here (cause list - circular doubly linked list)
+		sizeMyList = myList.size();
+		for (int i = 0; i < 3; i++) {
+			*iter = i + sizeMyList;
+			iter++;
+		}
+
+//		Change values & print them
+		iter = myList.begin();
+		while (iter != myList.end()) {
+			*iter = *iter - 3;
+			std::cout << *iter << std::endl;
+			iter++;
+		}
+
+		try {
+			std::cout << BOLD "Testing " GREEN "easyfind " STD BOLD "with list" STD << std::endl;
+			std::cout << *easyfind(myList, 0) << std::endl;
+			std::cout << *easyfind(myList, 6) << std::endl;
+			std::cout << *easyfind(myList, -3) << std::endl;
+			std::cout << *easyfind(myList, 100) << std::endl;
+			std::cout << *easyfind(myList, 1) << std::endl;
+		} catch (std::exception &exc) {
+			std::cerr << RED << exc.what() << STD << std::endl;
+		}
+	}
 
 	return 0;
 }
