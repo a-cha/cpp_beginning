14bb14727a1735485347ca25c4b65eb45f720d2a
Anton Chaparin <chaparin.anton@gmail.com>
2021-03-22 16:42:25 +0300

    add sbj tests

--- mod08/ex01/main.cpp
+++ mod08/ex01/main.cpp
@@ -8,6 +8,19 @@
 int		main() {
 	srand(time(nullptr));
 
+	{
+		std::cout << GREEN BOLD "Sbj tests" STD << std::endl;
+		Span sp = Span(5);
+		sp.addNumber(5);
+		sp.addNumber(3);
+		sp.addNumber(17);
+		sp.addNumber(9);
+		sp.addNumber(11);
+		std::cout << sp.shortestSpan() << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
+		std::cout << std::endl;
+	}
+
 	std::cout << GREEN BOLD "Test constructors" STD << std::endl;
 	std::cout << BOLD "Default + addNumber" STD << std::endl;
 	Span span(2);
