0f99fdcdc0773672bd758ba843b17f439758c66c
Anton Chaparin <chaparin.anton@gmail.com>
2021-03-22 22:02:07 +0300

    ad tests

--- mod08/ex02/main.cpp
+++ mod08/ex02/main.cpp
@@ -3,18 +3,29 @@
 //
 
 #include "MutantStack.hpp"
-#include <iostream>
+#include "print.hpp"
 
 int main() {
 	MutantStack<int> mutant;
 
+	std::cout << BOLD "Create MutantStack" STD << std::endl;
 	for (int i = 0; i < 5; i++) {
 		mutant.push(i * 10);
 	}
+	std::cout << GREEN "Print from begin iterator" STD << std::endl;
 	MutantStack<int>::iterator it = mutant.begin();
 	for (int i = 0; i < mutant.size(); i++) {
 		std::cout << *it << std::endl;
 		it++;
+	}
+	std::cout << GREEN "Remove 2 elems" STD << std::endl;
+	mutant.pop();
+	mutant.pop();
+	std::cout << GREEN "Print from end iterator" STD << std::endl;
+	it = mutant.end();
+	for (int i = 0; i < mutant.size(); i++) {
+		it--;
+		std::cout << *it << std::endl;
 	}
 	return 0;
 }
